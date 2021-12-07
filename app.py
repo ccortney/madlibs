7f1f95167c17bd645a0ff803f1f39ec4284eb1c0
Camilla Cortney <camilla.cortney@gmail.com>
2021-12-06 21:07:45 -0800

    further study

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 from flask import Flask, request, render_template
-import stories
-
-story = stories.story
+from stories import stories
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
+    return render_template("choice.html", stories=stories.values())
+
+@app.route('/form')
+def get_answers():
+    story_id = request.args["story_id"]
+    story = stories[story_id]
     prompts = story.prompts
-    return render_template("form.html", prompts = prompts)
+    return render_template("form.html", prompts = prompts,story_id=story_id)
 
 @app.route('/yourstory')
 def save_answers():
     answers = request.args
+    story_id = request.args['story_id']
+    story = stories[story_id]
     story_text = story.generate(answers)
     
     return render_template("yourstory.html", story_text=story_text)
--- stories.py
+++ stories.py
@@ -18,9 +18,9 @@ class Story:
         'I love to eat a good mango.'
     """
 
-    def __init__(self, words, text):
+    def __init__(self, title, words, text):
         """Create story with words and template text."""
-
+        self.title = title
         self.prompts = words
         self.template = text
 
@@ -34,12 +34,20 @@ class Story:
 
         return text
 
+story1 = Story("Once Upon a Time", ["place", "noun", "verb", "adjective", "plural_noun"], 
+        """Once upon a time in a long-ago {place}, there lived a
+       large {adjective} {noun}. It loved to {verb} {plural_noun}.""")
+story2 = Story("Party Invite!", ["name", "theme", "place", "time", "verb", "animal"], 
+        """{name} is having a {theme} party! It's going to be at {place}. 
+        Please make sure to show up at {time} or else you will be required to {verb}
+        with a {animal}.""")
+story3 = Story("New Company", ["company_name", "new_invention", "group_of_people", "problem_to_solve"],
+        """My company, {company_name}, is developing {new_invention}
+        to help {group_of_people} {problem_to_solve} with {secret sauce}!""")
+story4 = Story("Letter from Camp", ["family_member", "camp_name", "activity", "adjective", "plural_noun", "noun", "your_name"], 
+        """Dear {family_member}, Here I am at Camp {camp_name}! 
+        I am having a {adjective} time. My favorite thing to do so far
+        has been {activity}. I even saw a {noun}! I miss {plural_noun} though.
+        See you soon! Love, {your_name}""")
 
-# Here's a story to get you started
-
-
-story = Story(
-    ["place", "noun", "verb", "adjective", "plural_noun"],
-    """Once upon a time in a long-ago {place}, there lived a
-       large {adjective} {noun}. It loved to {verb} {plural_noun}."""
-)
+stories = {story.title: story for story in [story1, story2, story3, story4]}
